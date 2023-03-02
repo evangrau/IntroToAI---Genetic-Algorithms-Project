46185519b6b25dfff7927663473070d083d9aa33
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2023-03-02 15:26:33 -0600

    started fitness function

--- Algorithms/genetic_stocks.py
+++ Algorithms/genetic_stocks.py
@@ -20,15 +20,7 @@ def read_file(filename):
     return data
 
 
-def simple_moving_average(data, genotype):
-    window_size = 0
-    i = 0
-    for gene in genotype:
-        if gene == 's':
-            num = genotype[i + 1] + genotype[i + 2] + genotype[i + 3]
-            window_size = int(num)
-        i += 1
-
+def simple_moving_average(data, window_size):
     # Calculates the simple moving average of the data using the specified window size
     if len(data) < window_size:
         raise ValueError("Window size cannot be larger than data size")
@@ -38,23 +30,64 @@ def simple_moving_average(data, genotype):
         window = data[i-window_size:i]
         sma.append(sum(window) / window_size)
     
-    return sma
+    return sum(sma)
 
-def exponential_moving_average(data, genotype):
-    alpha = 0
-    i = 0
-    for gene in genotype:
-        if gene == 'e':
-            num = genotype[i + 1] + genotype[i + 2] + genotype[i + 3]
-            alpha = float(num) / 1000
-        i += 1
-
+def exponential_moving_average(data, alpha):
     # Calculates the exponential moving average of the data using the specified alpha value
     ema = [data[0]]
     for i in range(1, len(data)):
         ema.append(alpha * data[i] + (1 - alpha) * ema[-1])
     
-    return ema
+    return sum(ema)
+
+def max_rule(data, n):
+    if len(data) < n:
+        raise ValueError("Window size cannot be larger than data size")
+    
+    max_prices = []
+    for i in range(n, len(data)):
+        max_price = max(data[i-n:i])
+        max_prices.append(max_price)
+        
+    actions = []
+    for i in range(n, len(data)):
+        if data[i] > max_prices[i-n]:
+            actions.append('Buy')
+        else:
+            actions.append('Sell')
+            
+    return actions
+
+def fitness(genotype):
+    window_size = 0
+    i = 0
+    for gene in genotype:
+        if gene == 's':
+            num = genotype[i + 1] + genotype[i + 2] + genotype[i + 3]
+            window_size = int(num)
+        i += 1
+    alpha = 0
+    i = 0
+    for gene in genotype:
+        if gene == 'e':
+            num = genotype[i + 1] + genotype[i + 2] + genotype[i + 3]
+            alpha = 2 / int(num) + 1
+        i += 1
+    n = 0
+    i = 0
+    for gene in genotype:
+        if gene == 'm':
+            num = genotype[i + 1] + genotype[i + 2] + genotype[i + 3]
+            n = int(num)
+        i += 1
+    for dataset in datasets:
+        sma = simple_moving_average(dataset, window_size)
+        ema = exponential_moving_average(dataset, alpha)
+        max = max_rule(dataset, n)
+        print("Simple moving average     : " + str(sma))
+        print("Exponential moving average: " + str(ema))
+        print("Max rule                  : " + max[n])
+
 
 dir = "Datasets/historical-stock-prices/"
 data_strings = [dir+"AAPL-1.txt",dir+"AAPL-2.txt",dir+"AAPL-3.txt",dir+"AAPL-4.txt",dir+"AAPL-5.txt",dir+"DDS-1.txt",dir+"DDS-2.txt",dir+"DDS-3.txt",dir+"DDS-4.txt",dir+"DDS-5.txt",dir+"F-1.txt",dir+"F-2.txt",dir+"F-3.txt",dir+"F-4.txt",dir+"F-5.txt",dir+"GE-1.txt",dir+"GE-2.txt",dir+"GE-3.txt",dir+"GE-4.txt",dir+"GE-5.txt",dir+"RTX-1.txt",dir+"RTX-2.txt",dir+"RTX-3.txt",dir+"RTX-4.txt",dir+"RTX-5.txt"]
@@ -66,11 +99,7 @@ genotype = "s050&e030&m010"
 
 start_time = time.time()
 
-sma = sum(simple_moving_average(datasets[0], genotype))
-ema = sum(exponential_moving_average(datasets[0], genotype))
-
-print("Simple moving average     : " + str(sma))
-print("Exponential moving average: " + str(ema))
+fitness(genotype)
 
 end_time = time.time()
 time_elapsed = end_time - start_time
